ec031e531c3f0fb95700d4bac9e33d0a3d540e98
thabumon <52034689+thabumon@users.noreply.github.com>
2022-06-29 06:14:11 +0530

    Add files via upload

---
+++ main/views.py
@@ -0,0 +1,71 @@
+from django.http.response import JsonResponse
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+import pyttsx3
+
+# Create your views here.
+def main(request):
+    
+    if request.method == 'POST':
+        username =  request.POST['name']
+        email = request.POST['email']
+        password = request.POST['pwd']
+        
+        if User.objects.filter(email=email).exists():
+            return render(request,'signup.html',{'error':"Email already taken"}) 
+        else:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save();
+            print("user created")
+            return render(request,'login.html')
+    else:
+        return render(request,'signup.html') 
+
+
+def login_user(request):
+
+    if 'username' in request.session:
+        return render(request,'option.html')
+
+    if request.method == 'POST': 
+        username = request.POST.get('username')
+        password = request.POST.get('password') 
+
+        check_user = User.objects.filter(username=username, password=password)
+        if check_user:
+            return redirect("/login_user")
+        else:
+            request.session['username'] = username
+            return render(request,"login.html")
+
+    else:
+        return render(request,'login.html')
+
+def option(request):
+    if 'deaf' in request.POST:
+        return render(request,'deaf.html')
+    if 'dumb' in request.POST:
+        return render(request,'dumb.html')
+    if 'blind' in request.POST:
+        return render(request,"blind.html")
+
+
+def logout_user(request):
+    if 'username' in request.session:
+        request.session.flush()
+    return redirect('/')
+
+
+def dumb(request):
+    if 'username' in request.session:
+        return render(request,'dumb.html')
+    return redirect('/') 
+
+
+def txt_sp(request):
+    value=request.GET['text']
+    obj=pyttsx3.init()
+    obj.say(value)
+    obj.runAndWait()
+    return redirect('/dumb')
---
+++ speechText/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+
+    path('speechText', views.speechText, name='speechText'),
+    
+]
